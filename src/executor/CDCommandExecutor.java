ac7f5d074df0935b258c62518c24790c6ceaa554
Maruthi Adithya <maruthi.adithya@freshworks.com>
2018-10-06 11:28:10 +0530

    .. command support added
    - .. command support added

--- src/executor/CDCommandExecutor.java
+++ src/executor/CDCommandExecutor.java
@@ -1,4 +1,5 @@
 package executor;
+
 import java.util.List;
 import models.Directory;
 import service.DirectoryService;
@@ -9,43 +10,51 @@ public class CDCommandExecutor implements CommandExecutor {
 	public void execute(String args) {
 		DirectoryService directoryService = DirectoryService.getInstance();
 		Directory currentDirectory = directoryService.getCurrentDirectory();
-		if (!args.contains("/")) {
-			List<Directory> directories = directoryService.getCurrentDirectory().getChildren();
-			Directory found = null;
-			for (Directory directory : directories) {
-				if (directory.getName().equalsIgnoreCase(args)) {
-					found = directory;
-					break;
-				}
-			}
-			if (found != null) {
-				directoryService.setCurrentDirectory(found);
-				System.out.println("SUCC: REACHED");
-			} else {
-				System.out.println("ERR: INVALID PATH");
-			}
+		if (args.equalsIgnoreCase("..")) {
+			currentDirectory = currentDirectory.getParent();
+			directoryService.setCurrentDirectory(currentDirectory);
 		} else {
-			String[] directories = args.split("/");
-			int count = 0;
-			for (String name : directories) {
-				Directory directory = directoryService.getCurrentDirectory();
-				List<Directory> list = directory.getChildren();
-				for (Directory start : list) {
-					if (start.getName().equalsIgnoreCase(name)) {
-						count++;
-						directoryService.setCurrentDirectory(start);
+			if (!args.contains("/")) {
+				List<Directory> directories = directoryService.getCurrentDirectory().getChildren();
+				Directory found = null;
+				for (Directory directory : directories) {
+					if (directory.getName().equalsIgnoreCase(args)) {
+						found = directory;
 						break;
 					}
 				}
-			}
-			if (count == directories.length) {
-				System.out.println("SUCC: REACHED");
+				if (found != null) {
+					directoryService.setCurrentDirectory(found);
+					System.out.println("SUCC: REACHED");
+				} else {
+					System.out.println("ERR: INVALID PATH");
+				}
 			} else {
-				// Revert back to current Directory
-				directoryService.setCurrentDirectory(currentDirectory);
-				System.out.println("ERR: INVALID PATH");
+				String[] directories = args.split("/");
+				int count = 0;
+				for (String name : directories) {
+					Directory directory = directoryService.getCurrentDirectory();
+					List<Directory> list = directory.getChildren();
+					for (Directory start : list) {
+						if (start.getName().equalsIgnoreCase(name)) {
+							count++;
+							directoryService.setCurrentDirectory(start);
+							break;
+						}
+					}
+				}
+				if (count == directories.length) {
+					System.out.println("SUCC: REACHED");
+				} else {
+					// Revert back to current Directory
+					directoryService.setCurrentDirectory(currentDirectory);
+					System.out.println("ERR: INVALID PATH");
+				}
 			}
+
+
 		}
+
 	}
 
 }
